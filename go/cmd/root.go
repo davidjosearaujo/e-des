8241e1c1932b3171c28ec22654ff9be83d708f71
David Araújo <david2araujo5@gmail.com>
2023-10-22 00:06:50 +0100

    Rubik's Shuffle complete

--- go/cmd/root.go
+++ go/cmd/root.go
@@ -5,7 +5,9 @@ package cmd
 
 import (
 	"crypto/sha256"
+	"errors"
 	"fmt"
+	"math"
 	"os"
 
 	"golang.org/x/crypto/chacha20poly1305"
@@ -18,6 +20,59 @@ var (
 	Password string
 	Key      [32]byte
 )
+
+/* Rubik's Shuffle
+ * This shuffle technique is based on the way a Rubik cube is shuffled.
+ * Given a byte array with a perfect square length, and a shuffle key 
+ * of the same length as the root, the algorithm rotates each column
+ * the same number of places has indicated in the shuffle key at
+ * at the index indicated by the number of the column. This, obviously,
+ * will used half of the shuffle key.
+ * The row rotation follows exactly the same logic, but with 
+ * the rows and uses the second half of the shuffle key.
+ */	
+func rubikShuffle(matrix []byte, shuffleKey []int) ([]byte, error){
+	// Size of matrix
+	sideSize := int(math.Sqrt(float64(len(matrix))))
+	if sideSize*sideSize != len(matrix) {
+		return []byte{}, errors.New("it is now a square matrix")
+	}
+	
+	fmt.Printf("%02x\n", matrix)
+	fmt.Printf("Original length: %d\n\n", len(matrix))
+
+	// Rotate columns
+	for i:= 0; i < sideSize; i += 1{
+		temp := []byte{}
+		for j:= 0; j < len(matrix); j += sideSize {
+			temp = append(temp, matrix[i+j])
+		}
+		lastK := temp[(sideSize - shuffleKey[i]):sideSize]
+		firstSMK := temp[:(sideSize - shuffleKey[i])]
+		temp = append(lastK, firstSMK...)
+		for j,k := 0, 0; j < len(matrix); j, k = j+sideSize, k+1 {
+			matrix[i+j]=temp[k]
+		}
+	}
+
+	shuffleKey = shuffleKey[sideSize:]
+
+	// Rotate rows
+	for i,k := 0,0; i < len(matrix); i,k = i+sideSize, k+1 {
+		temp := matrix[i:i+sideSize]
+		lastK := temp[(sideSize - shuffleKey[k]):sideSize]
+		firstSMK := temp[:(sideSize - shuffleKey[k])]
+		temp = append(lastK, firstSMK...)
+		temp = append(matrix[:i], temp...)
+		matrix = append(temp, matrix[i+sideSize:]...)
+	}	
+
+	fmt.Printf("%02x\n", matrix)
+	fmt.Printf("Final length: %d\n", len(matrix))
+	os.Exit(0)
+
+	return matrix, nil
+}
 
 func keygen() {
 	Key = sha256.Sum256([]byte(Password))
@@ -36,19 +91,16 @@ func sboxgen() {
 	}
 
 	// Generate list of exchange indexes
-	nonce := make([]byte, chacha20poly1305.NonceSizeX)
-	ciphertext := aead.Seal(nil, nonce, make([]byte, 128*2), nil)
+	ciphertext := aead.Seal(nil, make([]byte, chacha20poly1305.NonceSizeX), make([]byte, 128*2), nil)
 	ciphertext = ciphertext[:256]
 
+	// Convert to shuffle key list
+	shuffleKey := []int{}
 	for i := 0; i < len(ciphertext); i += 1 {
-		num := (uint16(ciphertext[i]))
-		fmt.Printf("%d ", num%64)
+		shuffleKey = append(shuffleKey, int(ciphertext[i])%64)
 	}
 
-	// Rubik Cube manipulation
-	//	- Rotate columns
-	//	- Rotate rows
-
+	rubikShuffle(cleanbox, shuffleKey)
 }
 
 // rootCmd represents the base command when called without any subcommands
