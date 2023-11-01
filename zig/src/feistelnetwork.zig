f55c81eefed58b42630107b084a9a94586108fb3
David Araújo <david2araujo5@gmail.com>
2023-11-01 00:45:15 +0000

    Feistel network in separate module

---
+++ zig/src/feistelnetwork.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn EncFeistelNetwork(block: []u8, sbox: []u8) []u8 {
+    _ = block;
+    _ = sbox;
+}
+
+pub fn DecFeistelNetwork(block: []u8, sbox: []u8) []u8 {
+    _ = block;
+    _ = sbox;
+}
